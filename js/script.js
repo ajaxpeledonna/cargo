f0f0d05d2b9f7cda60dbab718e6a8272dde9be37
Michail Borovikov <ajapeledonna@gmail.com>
2021-11-03 12:03:57 +0300

    last changes germany

---
+++ js/script.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+  //the trigger on hover when cursor directed to this class
+    $(".core-menu li").hover(
+    function(){
+      //i used the parent ul to show submenu
+        $(this).children('ul').slideToggle('fast');
+    }, 
+      //when the cursor away 
+    // function () {
+    //     $('ul', this).slideUp('slow');
+    // }
+    );
+  //this feature only show on 600px device width
+    $(".hamburger-menu").click(function(){
+      $(".burger-1, .burger-2, .burger-3").toggleClass("open");
+        $(".core-menu").slideToggle("slow");
+    });
+
+
+	// Accordion  //
+
+$(function() {
+	
+	//BEGIN
+	$(".accordion__title").on("click", function(e) {
+
+		e.preventDefault();
+		var $this = $(this);
+
+		if (!$this.hasClass("accordion-active")) {
+			$(".accordion__content").slideUp(400);
+			$(".accordion__title").removeClass("accordion-active");
+			$('.accordion__arrow').removeClass('accordion__rotate');
+		}
+
+		$this.toggleClass("accordion-active");
+		$this.next().slideToggle();
+		$('.accordion__arrow',this).toggleClass('accordion__rotate');
+	});
+	//END
+	
+});
+
+
+$(".slider").slick({
+
+  // normal options...
+  infinite: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 5,
+  // the magic
+  responsive: [{
+
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 4,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
+
+$(".table-slider").slick({
+
+  // normal options...
+  infinite: false,
+  dots: true,
+  autoplay: false,
+  autoplaySpeed: 1000,
+  slidesToShow: 1,
+  slideToScroll: 1
+  
+});
+
+$(".main-slider").slick({
+  infinite: true,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 0,
+  arrows: true,
+  fade: true,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  slidesToShow: 1,
+  slideToScroll: 1
+});
+
+$(".route-slider").slick({
+  infinite: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 0,
+  arrows: true,
+  slidesToShow: 1,
+  slideToScroll: 1
+});
+
+$(".contact-slider").slick({
+  infinite: true,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slideToRight: true,
+  slideToScroll: 1
+});
+
+});
+
